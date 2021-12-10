2ea8f6e259d85b61b4be448109a8602f7a6ed82d
Max <pumpkinmaxpro@gmail.com>
2021-12-11 00:26:41 +0500

    add-main-slider

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+new Swiper('.main-slider', {
+    navigation: {
+        nextEl: '.slider-btn-next',
+        prevEl: '.slider-btn-prev',
+    },
+    effect: 'fade',
+    allowTouchMove: false,
+    loop: true,
+});
