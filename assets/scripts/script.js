11418e9e8dba7c668c6a35047be82a906a2ac8d6
Max <pumpkinmaxpro@gmail.com>
2021-12-23 08:10:06 +0500

    work has been done on the map and modal window

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -111,3 +111,105 @@ $("#video-item-modal-4").on('hidden.bs.modal', function (e) {
 $("#video-item-modal-5").on('hidden.bs.modal', function (e) {
     $("#video-item-modal-5 iframe").attr("src", $("#video-item-modal-5 iframe").attr("src"));
 });
+
+var regions=[
+    {
+        "region_name": "Karalpakistan Republic",
+        "region_code": "karalpakistanRepublic",
+    },
+    {
+        "region_name": "Xorazm",
+        "region_code": "xorazm",
+    },
+    {
+        "region_name": "Namangan",
+        "region_code": "namangan",
+    },
+    {
+        "region_name": "Andijon",
+        "region_code": "andijon",
+    },
+    {
+        "region_name": "Fergana",
+        "region_code": "fergana-2",
+    },
+    {
+        "region_name": "Fergana",
+        "region_code": "fergana",
+    },
+    {
+        "region_name": "Surhandarya",
+        "region_code": "surhandarya",
+    },
+    {
+        "region_name": "Sirdarya",
+        "region_code": "sirdarya",
+    },
+    {
+        "region_name": "Samarkand",
+        "region_code": "samarkand",
+    },
+    {
+        "region_name": "Kashkadarya",
+        "region_code": "kashkadarya",
+    },
+    {
+        "region_name": "Navoi",
+        "region_code": "navoi",
+    },
+    {
+        "region_name": "Djizzak",
+        "region_code": "djizzak",
+    },
+    {
+        "region_name": "Djizzak",
+        "region_code": "djizzak",
+    },
+    {
+        "region_name": "Bukhara",
+        "region_code": "bukhara",
+    },
+    {
+        "region_name": "Tashkent",
+        "region_code": "tashkent",
+    }
+];
+
+
+var temp_array= regions.map(function(item){
+    return item.population;
+});
+var highest_value = Math.max.apply(Math, temp_array);
+
+$(function() {
+
+    for(i = 0; i < regions.length; i++) {
+
+        $('#'+ regions[i].region_code)
+        .css({'fill': '#999999' + regions[i].population/highest_value +')'})
+        .data('region', regions[i]);
+    }
+
+    $('.where-to-buy__map path').click(function (e) {
+        var region_data=$(this).data('region');
+        $('<div class="info_panel">'+
+            region_data.region_name +
+          	'</div>'
+         )
+        .appendTo('body');
+        
+    })
+    // $('body').click(function() {
+    //     $('.info_panel').remove()
+    // })
+    .click(function(e) {
+        var mouseX = e.pageX, //X coordinates of mouse
+            mouseY = e.pageY; //Y coordinates of mouse
+        $('.info_panel').css({
+            top: mouseY-70,
+            left: mouseX - ($('.info_panel').width()/2)
+        });
+    });
+
+
+});
